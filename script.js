6889fda04cbef94caeb6d6c4a027b339a40bc31e
Amandeep Singh <samandeep8057@gmail.com>
2024-03-08 12:40:23 +0530

    update in script.js file, converted repeated code into drawing fns and done 2 mouse interactions click, move, and done some basic animations using requestAnimationFrame()

--- script.js
+++ script.js
@@ -3,34 +3,75 @@ canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 // console.log(canvas);
 const ctx = canvas.getContext("2d");
-console.log(ctx);
+// console.log(ctx);
 
-// drawing a filled Rectangle
-ctx.fillStyle = "green";
-ctx.fillRect(25, 25, 100, 100);
+// fn for drawing a filled Rectangle
+const drawFillRect = () => {
+  ctx.fillStyle = "green";
+  ctx.fillRect(mouse.x, mouse.y, 20, 20);
+};
 
-// drawing a filled Circle
-ctx.fillStyle = "orange";
-ctx.beginPath();
-ctx.arc(55, 180, 50, 0, 2 * Math.PI);
-ctx.fill();
+// fn for drawing a filled Circle
+const drawFillCircle = () => {
+  ctx.fillStyle = "orange";
+  ctx.beginPath();
+  ctx.arc(mouse.x, mouse.y, 50 / 5, 0, 2 * Math.PI);
+  ctx.fill();
+};
 
-// drawing a stroked Rectangle
-ctx.strokeStyle = "orange";
-ctx.beginPath();
-ctx.rect(150, 25, 100, 100);
-ctx.stroke();
+// fn for drawing a stroked Rectangle
+const drawStrokedRect = () => {
+  ctx.strokeStyle = "orange";
+  ctx.beginPath();
+  ctx.rect(mouse.x, mouse.y, 20, 20);
+  ctx.stroke();
+};
 
-// drawing a stroked Circle
-ctx.strokeStyle = "green";
-ctx.beginPath();
-ctx.arc(160, 180, 50, 0, 2 * Math.PI);
-ctx.stroke();
+// fn for drawing a stroked Circle
+const drawStrokedCircle = () => {
+  ctx.strokeStyle = "green";
+  ctx.beginPath();
+  ctx.arc(mouse.x, mouse.y, 50 / 4, 0, 2 * Math.PI);
+  ctx.stroke();
+};
 
-// Experimenting...
-ctx.beginPath();
-ctx.strokeStyle = "red";
-ctx.lineWidth = 1;
-ctx.moveTo(300, 25);
-ctx.lineTo(350, 125);
-ctx.stroke();
+// fn for drawing a line
+const drawLine = () => {
+  ctx.beginPath();
+  ctx.strokeStyle = "red";
+  ctx.lineWidth = 1;
+  ctx.moveTo(mouse.x, mouse.y);
+  ctx.lineTo(mouse.x + 5, mouse.y + 8);
+  ctx.stroke();
+};
+
+// mouse interactions
+
+// click interaction
+const mouse = {
+  x: undefined,
+  y: undefined,
+};
+
+canvas.addEventListener("click", (event) => {
+  mouse.x = event.x;
+  mouse.y = event.y;
+  // drawFillCircle();
+});
+
+// mouseMove interation
+canvas.addEventListener("mousemove", (event) => {
+  mouse.x = event.x;
+  mouse.y = event.y;
+  // drawFillCircle();
+});
+
+// animations
+const animate = () => {
+  // clearing canvas each pixels
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  drawFillCircle();
+  requestAnimationFrame(animate);
+};
+
+animate();
